bb7d9a4361275eefc6cb946689cbb8458ab18d7e
Amira-Argoubi <amiraargoubisimplon@gmail.com>
2020-05-04 01:34:01 +0100

    Add credit card

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+//import Card from "./card/card";
+//import Input from "./input/input";
+import Newinput from "./newinput/newinput";
+function App() {
+  return (
+    <div className="App">
+      <Newinput />
+    </div>
+  );
+}
+
+export default App;
